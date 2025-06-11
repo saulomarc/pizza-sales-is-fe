ea61d1ef382f2f3d8fb407a34b601313048783a7
saulomarc <mcsaulo@up.edu.ph>
2025-06-11 11:30:46 +0800

    chore: plugin vuedatepicker

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Notifications from 'notiwind'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
-
 pinia.use(piniaPluginPersistedState)
 app.use(pinia)
 app.use(router)
 app.use(Notifications)
+app.component('VueDatePicker', VueDatePicker)
 
 app.mount('#app')
